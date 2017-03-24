a21b548f253f01608729df9f28b65b99b3e80d14
Faith Mustapha <faith.mustapha@andela.com>
2017-03-24 15:47:40 +0100

    modify .gitignore file to include .DS_Store modify gulpfile.js, package.json,src/css/style.css to setup gulp on repo

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,50 @@
 /* File: gulpfile.js */
 
 // grab our gulp packages
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+var gulp = require( 'gulp' ),
+  gutil = require( 'gulp-util' ),
+  connect = require( 'gulp-connect' );
+
+
+
 
 // create a default task and just log a message
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
-});
+gulp.task( 'default', function () {
+  return gutil.log( 'Gulp is running!' )
+} );
+
+gulp.task( 'log', function () {
+  gutil.log( '== Gulp is Working ==' )
+} );
+
+gulp.task( 'watch', function () {
+  gulp.watch( 'src/css/style.css' );
+  gulp.watch( 'src/js/*.js' );
+  gulp.watch( 'src/index.html', [ 'html' ] );
+} );
+
+gulp.task( 'connect', function () {
+  connect.server( {
+    root: '.',
+    livereload: true
+  } )
+} );
+
+gulp.task( 'js', function () {
+  gulp.src( 'src/js/indexController.js', 'src/js/inverted.js' )
+    .pipe( connect.reload() )
+} );
+
+gulp.task( 'style', function () {
+  gulp.src( 'src/css/style.css' )
+    .pipe( connect.reload() )
+} );
+
+gulp.task( 'html', function () {
+  gulp.src( 'src/index.html' )
+    .pipe( connect.reload() )
+} );
+
+
+
+gulp.task( 'default', [ 'html', 'js', 'style', 'connect', 'watch' ] );
